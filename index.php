3f7383a4e8cadcf897080c752311ed9c779215ae
adtyekh11 <aditya.233040068@mail.unpas.ac.id>
2024-06-06 22:21:51 +0700

    medium 90%

--- index.php
+++ index.php
@@ -3,15 +3,12 @@
 <head>
   <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Tubes</title>
+      <title>Welcome</title>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
       <link rel="stylesheet" href="./asset/css/home.css">
   
 </head>
 <body>
-
-
-<!-- <a href="logout.php">Logout</a> -->
 
 <!-- navbar -->
 <nav class="navbar navbar-expand-lg">
